c4ac2cdd8607339771bde5e95e69ba7eb5ea09f5
xjxflyxx <79206657+xjxflyxx@users.noreply.github.com>
2021-02-18 02:09:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
 	name='jxcrawl', 				# 这是安装到 python 目录后的文件名，仅仅是文件名或目录名，不是包名，无法对这个名称进行 import；但如果用 twine upload dist/* 到了 pypi.org 的话，则此处是 pip install 要用到的包名，然后安装到 python 的 lib 之后，依然是文件名，无法用 import 引入。下面那个 packages 参数所指向的包名，才是可以 import 的。
-	version='1.1',
+	version='1.11',
 	author='xjxfly',
 	license='MIT',
 	#zip_safe = False, 				# 这个参数为 False ，使得安装到目标位置后是平坦的目录文件形式，而非 .egg 形式
@@ -13,7 +13,7 @@ setup(
 		'requests>=1',
 		'selenium>=1',
 		'beautifulsoup4>=1',
-		'jxbase>=2.20'
+		'jxbase>=2.20',
 		'cnstock>=1.1'
 		], 	# 这是本包要用到的依赖包
 	packages=['jxcrawl'], 			# 这是实际待安装的包名，即是个内含 __init__.py 的目录名，可以当作未来的包名引用，如 import pandas as pd 等，这里注意，子包必须带上父包前缀才能被找到安装，如 jxcrawl.sina 会被找到，但只写 sina 不会被找到，虽然他也是包名
